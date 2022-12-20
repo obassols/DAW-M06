c0c27bb3f9fddcd724dd5ed478dd273ca238b05d
oriol <obassols8@boscdelacoma.cat>
2022-12-20 19:35:25 +0100

    Add new valoration rows in criteria form

--- Angular/AtencioClient/src/app/Projecte/Components/form-criteria/form-criteria.component.ts
+++ Angular/AtencioClient/src/app/Projecte/Components/form-criteria/form-criteria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-criteria',
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-criteria.component.scss']
 })
 export class FormCriteriaComponent implements OnInit {
+  criteriaForm!: FormGroup;
 
-  constructor() { }
+  valorations: FormArray = this.fb.array([], [
+    Validators.minLength(2),
+  ]);
+
+  valorationsConfig = {
+    label: ['', [
+      Validators.required
+    ]],
+    puntuation: ['', [
+      Validators.required,
+      Validators.pattern('^[0-9]$')
+    ]]
+  };
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.addValoration();
+    this.addValoration();
+
+    this.criteriaForm = this.fb.group({
+      label: ['', [
+        Validators.required
+      ]],
+      valorations: this.valorations
+    });
   }
+
+  addValoration(): void {
+    this.valorations.push(this.fb.group(this.valorationsConfig));
+    console.log(this.valorations);
+  }
+
+  removeValoration(): void {
+    this.valorations.removeAt(this.valorations.length - 1);
+    console.log(this.valorations);
+  }
+
+  onSubmit(): void {
+    console.log(this.criteriaForm.value);
+  }
+
+  /* getControlValorationsLabel(): any {
+    return this.criteriaForm.get('valorations.label');
+  }
+
+  getControlValorationsPuntuation(): any {
+    return this.criteriaForm.get('valorations.puntuation');
+  } */
 
 }
